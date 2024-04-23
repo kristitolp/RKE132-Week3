fe9c9e154bf1c3445ec5f038827a5892a2a779d2
kristitolp <kristi.tolp@tktk.ee>
2024-04-23 18:46:21 +0300

    Week3 is ready

---
+++ IfDayOfTheWeek/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+
+if (weekDay == 0)
+{
+    Console.WriteLine("Its Sunday!");
+}
+
+else if (weekDay == 1)
+{
+    Console.WriteLine("Its Monday!");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("Its Tuesday!");
+}
+
+else if (weekDay == 3)
+{
+    Console.WriteLine("Its Wednesday!");
+}
+
+else if (weekDay == 4)
+{
+    Console.WriteLine("Its Thursday!");
+
+}
+
+else if (weekDay == 5)
+{
+    Console.WriteLine("Its Friday!");
+
+}
+
+else if (weekDay == 6)
+{
+    Console.WriteLine("Its Saturday!");
+
+}
---
+++ Switchlogic/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+Console.WriteLine("Mis hinde said?");
+
+
+
+string hinne = Console.ReadLine().ToUpper();
+
+switch (hinne)
+
+{
+    case "A":
+
+        Console.WriteLine("Suurepärane!");
+        break;
+
+    case "B":
+
+        Console.WriteLine("Väga hea!");
+        break;
+    case "C":
+
+        Console.WriteLine("Hea!");
+        break;
+    case "D":
+
+        Console.WriteLine("Rahuldav");
+        break;
+
+    case "E":
+
+        Console.WriteLine("Kasin");
+        break;
+
+    case "F":
+
+        Console.WriteLine("Puudulik");
+        break;
+
+    default:
+
+        Console.WriteLine("Vale väärtus");
+        break;
+}
